a1ddb749f2e88ee965b8c75f659c8d843901320b
Natividad Rodriguez <natividad.m1990@gmail.com>
2025-04-16 21:45:26 -0300

    estilizo navbar y agrego fuente

--- mi-portfolio/src/Components/Navbar/Navbar.jsx
+++ mi-portfolio/src/Components/Navbar/Navbar.jsx
@@ -2,18 +2,22 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import './Navbar.css'
+import { Link } from 'react-router-dom';
 
 function MyNavbar () {
+
+  
   return (
-    <Navbar expand="sm" className="MyNavbar">
-      <Container>
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-          <Nav.Link href="#home">Inicio</Nav.Link>
-            <Nav.Link href="#home">Sobre Mi</Nav.Link>
-            <Nav.Link href="#link">Skills</Nav.Link>
-            <Nav.Link href="#link">Portfolio</Nav.Link>
-            <Nav.Link href="#link">Contacto</Nav.Link>
+    <Navbar expand="sm" className="MyNavbar justify-content-center">
+      <Container className='justify-content-center'>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" className="ms-auto" />
+        <Navbar.Collapse id="basic-navbar-nav" className='justify-content-center'>
+          <Nav className="text-center">
+          <Link to="/" className="nav-link">Inicio</Link>
+            <Nav.Link href="#sobremi">Sobre Mi</Nav.Link>
+            <Nav.Link href="#skills">Skills</Nav.Link>
+            <Nav.Link href="#proyectos">Proyectos</Nav.Link>
+            <Nav.Link href="#contacto">Contacto</Nav.Link>
            </Nav>
         </Navbar.Collapse>
       </Container>
--- mi-portfolio/src/Components/PortfolioPage/PortfolioPage.jsx
+++ mi-portfolio/src/Components/PortfolioPage/PortfolioPage.jsx
@@ -18,7 +18,7 @@ const PortfolioPage = () => {
   
   return (
     <>
-    <section className="container">
+    <section className="container" id="sobremi">
      <div className="row">
       <div className='col-12 col-md-6 d-flex flex-column justify-content-center align-items-start' >
         <h2 className="mb-3">Sobre mi</h2>
@@ -27,17 +27,17 @@ const PortfolioPage = () => {
          <div className="social-links col-12 d-flex justify-content-center" style={{marginBottom: '15px'}}>
 
 <a href="mailto:tu-correo@gmail.com" target="_blank" rel="noopener noreferrer"  style={{ marginLeft: '10px', marginRight: '10px' }}>
-  <img src={linkedinIcon} alt="Linkedin" width="25" height="25" />
+  <img src={linkedinIcon} alt="Linkedin" width="25" height="25" className="icon-hover" />
   </a>
 
 
       <a href="https://github.com/Natividadrodriguez" target="_blank" rel="noopener noreferrer"  style={{ marginLeft: '10px', marginRight: '10px' }}>
-  <img src={githubIcon} alt="Github" width="25" height="25" />
+  <img src={githubIcon} alt="Github" width="25" height="25" className="icon-hover" />
   </a>
       
 
       <a href="mailto:natividad.m1990@gmail.com" target="_blank" rel="noopener noreferrer"  style={{ marginLeft: '10px', marginRight: '10px' }}>
-  <img src={gmailIcon} alt="Gmail" width="32" height="25" />
+  <img src={gmailIcon} alt="Gmail" width="32" height="25" className="icon-hover" />
   </a>
 </div>
       </div>
@@ -50,7 +50,7 @@ const PortfolioPage = () => {
   
     </section>
 
-    <section className="container">
+    <section className="container" id="skills">
       <div className="row">
         <h2 className="mb-3">Skills</h2>
         <p>Estas son las tecnologías que he ido aprendiendo hasta ahora. Actualmente me encuentro en formación continua, ampliando mis conocimientos día a día, con el objetivo de crecer como desarrolladora y sumar nuevas herramientas a mi perfil.</p>
@@ -67,7 +67,7 @@ const PortfolioPage = () => {
       </div>
     </section>
 
-    <section className="container">
+    <section className="container" id="proyectos">
       <div className="row">
         <h2 className="mb-5">Proyectos</h2>
         <div className="d-flex flex-column flex-md-row justify-content-center align-items-center flex-wrap">
@@ -152,24 +152,26 @@ const PortfolioPage = () => {
       
     </section>
 
-    <section className="container">
+    <section className="container" id="contacto">
       <div className="row">
         <h2 className="mb-5">Contacto</h2>
 <div className='col-12 col-md-4 text-center mb-4'>
-<img src={gmailIcon} alt="Gmail" width="40" height="32" className="mb-2" />
+<a href="mailto:natividad.m1990@gmail.com" target="_blank" rel="noopener noreferrer">
+        <img src={gmailIcon} alt="Gmail" width="38" height="32" className="mb-2 icon-hover" />
+      </a>
 <p>natividad.m1990@gmail.com</p>
 </div>
 
 <div className='col-12 col-md-4 text-center mb-4'>
 <a href="http://www.linkedin.com/in/rodrigueznatividad" target="_blank" rel="noopener noreferrer">
-        <img src={linkedinIcon} alt="LinkedIn" width="32" height="32" className="mb-2" />
+        <img src={linkedinIcon} alt="LinkedIn" width="32" height="32" className="mb-2 icon-hover" />
       </a>
       <p>/rodrigueznatividad</p>
 </div>
 
 <div className='col-12 col-md-4 text-center mb-4'>
 <a href="https://github.com/Natividadrodriguez" target="_blank" rel="noopener noreferrer">
-        <img src={githubIcon} alt="GitHub" width="32" height="32" className="mb-2" />
+        <img src={githubIcon} alt="GitHub" width="32" height="32" className="mb-2 icon-hover" />
       </a>
       <p>/Natividadrodriguez</p>
 </div>
