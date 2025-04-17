f793d935bae492d33c064706c95e1fff2c8bd5b8
Natividad Rodriguez <natividad.m1990@gmail.com>
2025-04-16 22:55:03 -0300

    solucionando problemas de deploy

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import HomePage from "./Components/HomePage/HomePage";
 import Carrousel from "./Components/Carrousel/Carrousel"; 
 import PortfolioPage from "./Components/PortfolioPage/PortfolioPage";
 import MyNavbar from "./Components/Navbar/Navbar";
-import './app.css'
+import './App.css'
 
 const App = () => {
   return (
